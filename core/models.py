f098d19ab3bb25a8fd01a8b8cdabe2a3b8c2b4a2
Vinod Kumar <vinodrao835@gmail.com>
2024-11-03 16:48:16 +0530

    first commit

---
+++ core/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+import uuid
+from django.conf import settings
+
+# Create your models here.
+
+class Movie(models.Model):
+
+    GENRE_CHOICES = [
+        ('action', 'Action'),
+        ('comedy', 'Comedy'),
+        ('drama', 'Drama'),
+        ('horror', 'Horror'),
+        ('romance', 'Romance'),
+        ('science_fiction', 'Science Fiction'),
+        ('fantasy', 'Fantasy'),
+    ]
+
+    uu_id = models.UUIDField(default=uuid.uuid4)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    genre = models.CharField(max_length=100, choices=GENRE_CHOICES)
+    length = models.PositiveIntegerField()
+    image_card = models.ImageField(upload_to='movie_images/')
+    image_cover = models.ImageField(upload_to='movie_images/')
+    video = models.FileField(upload_to='movie_videos/')
+    movie_views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+# 2 foreign keys
+class MovieList(models.Model):
+    # on_delete=models.CASCADE means that if the user associated with a MovieList entry is deleted, 
+    # all MovieList records associated with that user will also be deleted.
+    owner_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    
+
